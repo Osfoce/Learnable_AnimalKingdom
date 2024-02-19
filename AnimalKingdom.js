c724ca0205b4a29c6c6c8e96dd0dc4c6d67138de
Osfoce <oselufortunatus2002@gmail.com>
2024-02-19 14:45:49 +0100

    Animal Kingdom prototype

---
+++ AnimalKingdom.js
@@ -0,0 +1,40 @@
+class AnimalKingdom {
+    constructor(){
+        if(this.constructor == AnimalKingdom)
+        {
+            throw new Error('I am the boss... and immutable')
+        }
+    }
+    vertebrates(){
+        this.hasBackBone = true;
+        this.hasEndoSkeleton = true;
+        this.isMammal = true;
+    }
+    inVertebrates(){
+        hasBackBone = false;
+        hasEndoSkeleton = false;
+        isMammal = false;
+    }
+    coldBlodded(){
+        isAquatic = true;
+
+    }
+    warmBlodded(){
+        isAquatic = false;
+    }
+}
+
+class Mammal extends AnimalKingdom {
+    vertebrates(){
+        this.hasBackBone = true;
+        this.hasEndoSkeleton = true;
+        this.isMammal = true;
+    }
+    warmBlodded(){
+        isAquatic = false;
+    }
+}
+
+const peter = new Mammal
+
+console.log(peter)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Learnable_AnimalKingdom
+# Learnable-AnimalKingdom
+## Objective: Model the Animal kingdom above as a class. Apply all four principles of OOP.
